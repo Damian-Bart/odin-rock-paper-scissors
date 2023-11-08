4eb01fea4eae8d9629fa8baf2aadf6eb066079e3
Damian-Bart <dbart99@proton.me>
2023-11-08 09:43:00 +0000

    Add else if block if user looses

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // Create array with available choices //
 const choices = ['rock', 'paper', 'scissors']
+
 // Create a function to get computer choice. Rock/Paper/Scissors //
 function getComputerChoice() {
+
     // Get random index from the array //
     const computerChoice = choices[Math.floor(Math.random()* choices.length)];
     return computerChoice;
 }
-// Ask user for their choice //
-function getUserChoice () {
-    let userChoice = prompt('Please choose rock/paper/scissors: '); 
+function playRound(userChoice, computerChoice) {
+    
+    // Ask user for their choice //
+    let userChoice = prompt('Please choose rock/paper/scissors: ').toLowerCase(); 
     return userChoice;
-}
 
-console.log(getUserChoice())
-// Compare user's choice to computer and print result //
+    // Save computer choice to a variable //
+    computerChoice = getComputerChoice();
+    
+    if (userChoice === computerChoice) {
+        return "It's a tie!"
+    } else if (userChoice === 'rock' && computerChoice === 'paper') {
+        return "You Lose! Paper beats rock!"
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        return "You Win! Paper beats rock!"
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        return 
+    }
+    // Compare user's choice to computer and print result //
+}

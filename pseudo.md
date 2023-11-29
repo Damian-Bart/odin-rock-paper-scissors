4cb50e588fbaca63adc205a1e9d0d7390d027958
Damian-Bart <dbart99@proton.me>
2023-11-29 15:11:13 +0000

    Created simple UI with buttons and scoreboard

---
+++ pseudo.md
@@ -0,0 +1,10 @@
+Create array with available choices
+Create a function to get computer choice
+    Get random index from the choices array
+    Return the choice
+Get user choice from the click of the button they selected
+Create playRound function
+    Compare user's and computer choice
+    Add points accordingly
+Play rounds until someone gets 5 wins
+Show the winner
--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-// Create array with available choices //
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ["rock", "paper", 'scissors'];
+let computerScore = 0;
+let userScore = 0;
+let winnerBoard = "Choose one option from the buttons above"
 
-
-// Create a function to get computer choice. Rock/Paper/Scissors //
 function getComputerChoice() {
-
-    // Get random index from the array //
-    const computerChoice = choices[Math.floor(Math.random()* choices.length)];
-    return computerChoice;
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+function updateScores() {
+    userScoreHTML.textContent = userScore;
+    computerScoreHTML.textContent = computerScore;
+    winnerBoardHTML.textContent = winnerBoard;
 }
 
-
-
-// Save computer choice to a variable //
-const computerChoice = getComputerChoice();
-
-// Create a function to play a round //
 function playRound(userChoice, computerChoice) {
-    
-     // Compare user's choice to computer and print result //
     if (userChoice === computerChoice) {
-        return "It's a tie!";
+        winnerBoard = "It's a tie!";
     } else if (
-    (userChoice === 'rock' && computerChoice === 'paper') ||
-    (userChoice === 'paper' && computerChoice === 'scissors') ||
-    (userChoice === 'scissors' && computerChoice === 'rock')) {
-        return `You Lose! ${computerChoice} beats ${userChoice}`;
-    } else {
-        return `You Win! ${userChoice} beats ${computerChoice}`;
-    }   
+        (userChoice === "rock" && computerChoice === "paper") ||
+        (userChoice === "paper" && computerChoice === "scissors") ||
+        (userChoice === "scissors" && computerChoice === "rock")
+    ){
+        computerScore += 1;
+        winnerBoard = `You lose, ${computerChoice} beats ${userChoice}!`;
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ){
+        userScore += 1;
+        winnerBoard = `You win, ${userChoice} beats ${computerChoice}!`;
+    }
+    updateScores()
 }
 
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const userScoreHTML = document.getElementById("userScore");
+const computerScoreHTML = document.getElementById("computerScore");
+const winnerBoardHTML = document.getElementById("winnerBoard");
 
-const buttons = document.querySelectorAll('button');
-const rockBtn = document.getElementById('rock');
-const paperBtn = document.getElementById('paper');
-const sciBtn = document.getElementById('scissors');
+rockBtn.addEventListener('click', () => playRound("rock", getComputerChoice()));
+paperBtn.addEventListener('click', () => playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener('click', () => playRound("scissors", getComputerChoice()));
 
-rockBtn.addEventListener('click', playRound('rock', computerChoice));
-paperBtn.addEventListener('click', playRound('paper', computerChoice));
-sciBtn.addEventListener('click', playRound('scissors', computerChoice));
+updateScores()
